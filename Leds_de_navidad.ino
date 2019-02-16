d2e7d28ce3ad3480fd0adaaee1922d54aefff60c
Juvenal <38957852+JuvsProgramer@users.noreply.github.com>
2019-02-16 11:52:22 -0600

    Add files via upload

---
+++ Leds_de_navidad.ino
@@ -0,0 +1,27 @@
+int ledsArray[] = {1,2,3,4,5,6,7,8};
+int count = 0;
+
+
+
+void setup() {
+for(count = 0; count < 8; count++)
+{pinMode (ledsArray[count], OUTPUT);}
+}
+
+void loop() {
+  
+ for(count = 0; count < 8; count++){
+  digitalWrite(ledsArray[count],HIGH);
+  delay(3);
+digitalWrite(ledsArray[count],LOW);
+  delay(3);
+ }
+  if(count == 8){
+  for(count = 8; count > 0; count--){
+  digitalWrite(ledsArray[count],HIGH);
+  delay(3);
+digitalWrite(ledsArray[count],LOW);
+  delay(3);
+  }}}
+
+
